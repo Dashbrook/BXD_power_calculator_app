3e5172c7f702f0fb733011322f25f105aec7dbbd
Dashbrook <dashbrook@uthsc.edu>
2019-01-14 10:56:44 -0600

    Added in comments on ui.R and server.R, removed some excess code.

--- server.R
+++ server.R
@@ -2,73 +2,72 @@
 #
 library(shiny)
 library(datasets)
-library(ggplot2)# load ggplot
+library(ggplot2)
 library(RColorBrewer)
 library(data.table)
 library(dplyr)
 library(DT)
 
-## import data
+## import data from website, pre-calculated so once its loaded changing inputs rapidly changes the plots. Takes a little time for initial loading though. 
 dataimported = read.csv(url("http://individual.utoronto.ca/D_Ashbrook/Effect_size_analysis_heritability_28th_Nov_2018_recalc.csv"), header = TRUE)
 
-# Define server logic required to plot various variables against mpg
+# Define server logic required to plot various variables
 shinyServer(function(input, output) {
+  ## Create a table of values, using the user defined inputs to subset the full table. This will be pulled in later to provide the user with a table of values.
   Significant_lines_table <- reactive({(subset(dataimported,effect.size.locus==input$a&Heritability==input$b&Power>=input$c&NumberOfReplicates>=input$slider2[1]&NumberOfReplicates<=input$slider2[2])[,c(1,3,4,7)])%>% 
       group_by(numberoflines) %>% 
       slice(which.min(NumberOfReplicates))
   })
-  
+  ## The main plot
   output$powerplot <- renderPlot({
-    
+    ## ggplot, using the user inputs
     ahhhh <- ggplot(data=dataimported, aes(x=numberoflines, y=Power, group=factor(NumberOfReplicates), colour=factor(NumberOfReplicates))) + 
+      # Subset the data based on user inputs
       geom_line(data=subset(dataimported,effect.size.locus==input$a&Heritability==input$b&NumberOfReplicates>=input$slider2[1]&NumberOfReplicates<=input$slider2[2])) +
+      # Put horizontal line, at the users chosen poser
       geom_hline(yintercept = input$c) +
-      #      geom_point(data=subset(dataimported,NumberOfReplicates==input$a&Heritability==input$b&Power<=0.95))+ 
+      # Define y axis (so all plots look the same, and can be compared)
       scale_y_continuous(breaks=seq(0,1,0.05), limits = c(0, 1), expand=c(0,0)) + 
+      # Define x axis (so all plots look the same, and can be compared)
       scale_x_continuous(breaks=seq(0,150,10), limits = c(0,150), expand=c(0,0)) +
+      # Define theme, e.g. size of axis labels
       theme(legend.position="top", axis.text=element_text(size=14),
             axis.title=element_text(size=18,face="bold")) + 
       guides(colour = guide_legend(override.aes = list(size=3))) +
+      # Label axis
       xlab("Number of RI strains") + 
       ylab("Power") + 
       labs(color = "Number of biological replicates\n")
     print(ahhhh)
   })
-  output$summary <- renderPrint({
-    Significant_lines_table()
-  })
-  output$view <- renderTable({
-    (Significant_lines_table())
-  })
-  
-  output$mytable = DT::renderDataTable({
-    Significant_lines_table
-  })
+  ## Output table, showing the combinations of number of lines and number of biological replicates sufficient to reach the user selected power 
   output$tbl2 <- DT::renderDataTable({
     DT::datatable(Significant_lines_table(),options = list(pageLength = 10), colnames = c('Power', 'Number of strains', 'Number of biological replicates', 'Total animals needed'))%>%
       formatRound(columns=c('Power'), digits=2)
   })
+  ## Output effect size plot, showing the theoretical distribution of values, given the selected locus effect size and number of bioloigcal replicates
   output$effectplot <- renderPlot({
-    
-    n<-input$slider2[1]            ## Sample n individuals at a time
-    effect <- input$a
-    p_mean_1<- -((input$a)/2)        ## Population mean
-    p_mean_2<- (input$a)/2        ## Population mean
-    p_sd<-1            ## Population standard deviation
+    n<-input$slider2[1]            ## Sample n individuals at a time - taken from selected minimum number of biological replicates
+    effect <- input$a ## Locus effect size, as selected by user
+    p_mean_1<- -((input$a)/2)        ## Population mean of the homozygote with low values (negative, half of the total locus effect size)
+    p_mean_2<- (input$a)/2        ## Population mean of the homozygote with high values (positive, half of the total locus effect size)
+    p_sd<-1            ## Population standard deviation - define the standard deviation as 1
+    ## Plot the whole population curve, i.e. by adding the values for both homozygote populations. Define size of the plot, so all three curves will fit on same axis
     vc <-  curve(((dnorm(x,p_mean_2,p_sd/sqrt(n)))+((dnorm(x,p_mean_1,p_sd/sqrt(n))))), xlim=c(-5,5), ylim=c(0,3.5), ylab = "Density", xlab = "")
     title(main="Standard error density around the mean of the two homozoygotes
             using effect size and minimum number of replicates inputs")
+    ## Plot curve for the low value homozygote
     va <-  curve(dnorm(x,p_mean_1,(p_sd/sqrt(n))), xlim=c(-5,5), ylim=c(0,3.5),add=TRUE) 
+    ## Plot curve for the high value homozygote
     vb <-  curve(dnorm(x,p_mean_2,(p_sd/sqrt(n))), xlim=c(-5,5), ylim=c(0,3.5),add=TRUE)
+    ## Write the two values used to calculate the plot (already selected by user, but here to remind them)
     text(3,3, labels = ("Locus effect size:"))
     text(3,3, labels = (input$a), offset	= 3.5, pos = 4)
     text(3,2.5, labels = ("Number of replicates:"))
     text(3,2.5, labels = (input$slider2[1] ), offset	= 4, pos = 4)
     
   })
-  ## Need to get H2RIX for the selected H2, and selected number of strains
-  ## prob need to do some subsetting, then output, rather than just output. 
-  ## Should be able to use the significant lines table
+  ## Calculate the H2RIX using the smallest and largest of the selected number of bioloigcal replicates. 
   output$selected_var_1 <- renderText({    paste("H2RI using minimum biological replicates value:", (signif((min(subset(dataimported, effect.size.locus==input$a&Heritability==input$b&NumberOfReplicates==input$slider2[1])$H2RI)),3)))})
   output$selected_var_2 <- renderText({    paste("H2RI using maximum biological replicates value:", (signif((min(subset(dataimported, effect.size.locus==input$a&Heritability==input$b&NumberOfReplicates==input$slider2[2])$H2RI)),3)))})
 })
--- ui.R
+++ ui.R
@@ -2,38 +2,49 @@
 #
 library(shiny)
 
+## Load UI
 shinyUI(fluidPage(tags$style(type = "text/css", ".irs-grid-pol.small {height: 0px;}"),
                   fluidRow(
+                    # Create title, link to help page, etc, across whole of top
                     column(12, offset = 0.2,
                            h1("Power Calculator for a two parent recombinant inbred family"),h2("Probability (beta) of detecting a QTL as a function of heritability, strains, and replicates within strain", offset = 3),tags$div(class="header", checked=NA, tags$a(href="https://davidashbrook.wordpress.com/bxd-power-app-help-page/", "     (Help Page)")),h3("     Beta 0.7; 10th January 2019"),h4("     David Ashbrook and colleagues")),
+                    # Create inputs, half width of title
                     column(6,
+                           # Heritability input, with link to help page
                            tags$a(href="https://davidashbrook.wordpress.com/bxd-power-app-help-page#Heritability", "Heritability"),
+                           # Input for heritability, slider between 0 and 1.
                            sliderInput("b", label = NULL, 
                                        min=0, max=1, value=0.1, step = 0.05, ticks = TRUE, width = '90%'),
-                           #           
-                           #           # Allele effect,size.allele
+                           # Locus effect size input, and link to help file
                            tags$a(href="https://davidashbrook.wordpress.com/bxd-power-app-help-page#Standardized-locus-effect-size", "Standardized locus effect size"),
+                           # Input for locus effect size, slider between 0 and 1
                            sliderInput("a", label = NULL, 
                                        min = 0, max = 1, value = 1, step = 0.05, ticks = TRUE, width = '90%'),
+                           # Number of biological replicates (within-strain replicates), and link to help file
                            tags$a(href="https://davidashbrook.wordpress.com/bxd-power-app-help-page#Within-strain replicates", "Biological replicates"),
+                           # Input for number of biological replicates, between 1 and 10. 
                            sliderInput("slider2", label = NULL, min = 1, 
                                        max = 20, value = c(2, 10), ticks = TRUE, width = '90%'),
+                           # H2RIX, help file
                            tags$a(href="https://davidashbrook.wordpress.com/bxd-power-app-help-page#H2RIX", "H2RI"),
+                           # Minimum H2RIX (using the smallest number of biological replicates selected)
                            textOutput("selected_var_1"),
+                           # Maximum H2RIX (using the largest number of biological replicates selected)
                            textOutput("selected_var_2"),
-                           
-                           
-                           #           # Power
+                           # Power input, with link to help file
                            tags$a(href="https://davidashbrook.wordpress.com/bxd-power-app-help-page#Power", "Power threshold"),
+                           # Input for power (i.e. the cut off value the user wants), between 0.1 and 1 (power of 0 is meaningless)
                            sliderInput("c", label = NULL, 
                                        min = 0.1, max = 1, value = 0.8, step = 0.05, ticks = TRUE, width = '90%')),
-                    
+                    # Create space for the effect plot, the other half of the page next to inputs
                     column(6,
                            plotOutput("effectplot"))
                   ),
+                  # Move to next row, create plot for power vs locus effect size, the main plot. Just less than full size (11.5) to allow some white space
                   fluidRow(
                     column(11.5, offset = 0.2, textOutput("Power vs Locus Effect Size"),
                            plotOutput("powerplot", height = 600))),
+                  # Move to next row, create table to find strain combinations (i.e. number of strains and number of biological replicates) sufficient to get above the selected power cut-off
                   fluidRow(
                     column(3, offset = 0.2, 
                            verbatimTextOutput("Strain combinations with sufficient power"),
