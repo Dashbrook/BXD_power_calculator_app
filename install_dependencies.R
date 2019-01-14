bab237677c1e04e647a899825ca58363a204e6b9
DannyArends <Danny.Arends@Gmail.com>
2019-01-14 17:10:03 +0100

    Small script to install the dependencies

---
+++ install_dependencies.R
@@ -0,0 +1,9 @@
+#
+# install_dependencies.R - Installation of the dependencies
+#
+# Author: Danny Arends
+# last modified Jan, 2019
+# first written Jan, 2019
+#
+
+install.packages(c("shiny", "ggplot2", "RColorBrewer", "dplyr", "data.table", "DT"))
